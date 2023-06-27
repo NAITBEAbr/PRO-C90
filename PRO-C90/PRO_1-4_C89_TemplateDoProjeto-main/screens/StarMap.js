628e8f9c937cdce4cd7ba414a93a0e33a7603390
NAITBEAbr <100884994+NAITBEAbr@users.noreply.github.com>
2023-06-27 19:18:20 -0300

    Add files via upload

--- PRO-C90/PRO_1-4_C89_TemplateDoProjeto-main/screens/StarMap.js
+++ PRO-C90/PRO_1-4_C89_TemplateDoProjeto-main/screens/StarMap.js
@@ -61,11 +61,7 @@ class Cordenada {
     }
   }
   
-  const {longitude,latitude} = this.state;
-  const path = `https://virtualsky.Ico.global/embed/index.html?longitude=
-  ${longitude}&latitude=${latitude}&constellations=true&constellationlabels=
-  true&showstarlabels=true&gridlines_az=true&live=true&projection=stereo&showdate=
-  false&showposition=false`
+  
 
 
 
